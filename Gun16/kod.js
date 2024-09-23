5fb5c66c647c822f5b5f935ca72ad3f1935d196f
feyzadeniz <keskinfeyza28@gmail.com>
2024-09-23 22:19:28 +0300

    js giriş

---
+++ Gun16/kod.js
@@ -0,0 +1,2 @@
+let baslik = document.querySelector("h1"); /* tek tırnak da makbul */
+baslik.style.textTransform = "uppercase "; /* normaldetext-transform şeklinde yazarken burada textTransform şeklinde yazıyoruz */
